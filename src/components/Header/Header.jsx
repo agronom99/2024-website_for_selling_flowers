81cdbbbb41a6fb247cb6d8299a338f74a2b1b1a4
Vitalii MV <agronom9913@gmail.com>
2024-02-04 17:49:06 +0200

    2024.02.04 start

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import "./heder.scss"
+ 
+const Header = () => {
+  return (
+    <div className="header h-12">
+        <nav className="">
+            55
+        </nav>
+      
+    </div>
+  )
+}
+
+export default Header
