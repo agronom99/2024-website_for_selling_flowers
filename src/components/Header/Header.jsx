4141370589cc5768ee97fa81b083bb5181801b34
Vitalii MV <agronom9913@gmail.com>
2024-02-04 20:49:15 +0200

    2024.02.04 header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,26 @@
-import "./heder.scss"
+import "./heder.scss";
+import Entrance from '../../components/images/entrance.svg'
  
 const Header = () => {
   return (
-    <div className="header h-12">
-        <nav className="">
-            55
+    <div className="header text-base">
+        <nav className=" flex items-center place-content-around">
+            <section className=" flex w-[899px] place-content-around" >
+              <div>Валюта</div>
+              <div>Мова</div>
+              <div>Місто</div>
+              <div>Закладки</div>
+              <div>Доставка та оплата</div>
+              <div>Контакти</div>
+            </section>
+            <section className=" flex w-40 place-content-around">
+              <div className=" flex" > 
+                <img src={Entrance} alt="entrance" className=" w-4 h-4 mr-2 my-auto"/>
+                <p>Вхід</p>
+              </div>
+              <div>| Реєстрація</div>
+            </section>
+
         </nav>
       
     </div>
